b3826217e2986f6ae3567b215cd50e15a9655470
Gerson Penha <Gerson Penha@DESKTOP-VREDC3F>
2020-03-06 09:32:18 -0300

    algo

--- 2020S1-1/src/br/com/app/fatec/App.java
+++ 2020S1-1/src/br/com/app/fatec/App.java
@@ -1,12 +1,35 @@
 package br.com.app.fatec;
 
 import br.com.negocio.fatec.AsciiArt;
+import br.com.negocio.fatec.Controle;
 import br.com.negocio.fatec.Menu;
 
 public class App {
 	public static void main(String[] args) {
 		AsciiArt.printAsciiArt("Serasa");
 		Menu menu = new Menu();
-		menu.imprimirMenu();
+		int op = 1000;
+		while (op != 0) {
+			menu.imprimirMenu();
+			Controle ctl = new Controle();
+			op = ctl.opcao();
+			switch (op) {
+			case 1:
+				// Aqui que voces vão se virar nos 30
+				break;
+
+			default:
+				menu.imprimirMenu();
+				break;
+			}
+		}
+
 	}
 }
+
+
+
+
+
+
+
